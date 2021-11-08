e72453cd262e0de8a2a7ea4fc74374d399798ed7
Carlos Mata <carlosmata1009@gmail.com>
2021-11-08 19:46:34 +0100

    cambios

--- App/app/src/main/java/com/entertainment/app/MainActivity.kt
+++ App/app/src/main/java/com/entertainment/app/MainActivity.kt
@@ -2,10 +2,25 @@ package com.entertainment.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("carlos","Creating app")
+
+    }
+    override fun onResume(){
+        super.onResume()
+        Log.d("carlos","App on resume")
+    }
+    override fun onPause(){
+        super.onPause()
+        Log.d("carlos","App paused")
+    }
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.d("carlos","Destroying")
     }
 }
