de2e05945c20cb124bd64d1a30787e2860d41f8a
Carlos Mata <carlosmata1009@gmail.com>
2021-11-20 21:28:51 +0100

    cambios

--- App/app/src/main/java/com/entertainment/app/MainActivity.kt
+++ App/app/src/main/java/com/entertainment/app/MainActivity.kt
@@ -3,13 +3,18 @@ package com.entertainment.app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    var button:Button?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main);
+        supportActionBar?.hide();//Quitar barra.
         Log.d("carlos","Creating app")
-
     }
     override fun onResume(){
         super.onResume()
