5eed81d3cb59c0d42d842a907decf42cc48941f9
Carlos Mata <carlosmata1009@gmail.com>
2021-11-21 16:22:33 +0100

    cambios

--- App/app/src/main/java/com/entertainment/app/MainActivity.kt
+++ App/app/src/main/java/com/entertainment/app/MainActivity.kt
@@ -1,24 +1,24 @@
 package com.entertainment.app
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.MediaController
+import android.widget.VideoView
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()//Delete bar
         val button = findViewById<Button>(R.id.button5)
-        button.setOnClickListener(){
-            val Intent = Intent(this,MainActivity2::class.java)
-            startActivity(Intent)
+        button.setOnClickListener{
+            val intent = Intent(this,MainActivity2::class.java)
+            startActivity(intent)
         }
-        Log.d("carlos","Creating app")
-
     }
 
     override fun onResume(){
--- App/app/src/main/java/com/entertainment/app/MainActivity2.kt
+++ App/app/src/main/java/com/entertainment/app/MainActivity2.kt
@@ -4,13 +4,11 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.MediaController
-import android.widget.Toast
 import android.widget.VideoView
 class MainActivity2 : AppCompatActivity() {
-    private var videoPlayer:VideoView?=null
-    private var videoController:MediaController?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
@@ -18,30 +16,12 @@ class MainActivity2 : AppCompatActivity() {
         button.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-        }
-        videoPlayer=findViewById<VideoView>(R.id.videoView)
-        configVideoPlayer()
-    }
-    private fun configVideoPlayer(){
-        if(videoPlayer==null){
-            videoController= MediaController(this)
-            videoController!!.setAnchorView(this.videoPlayer)
-        }
-        videoPlayer!!.setMediaController(videoController)
-        videoPlayer!!.setVideoURI(
-            Uri.parse(    "android.resource://$packageName/${R.raw.american}")
-        )
-        videoPlayer!!.requestFocus()
-        videoPlayer!!.pause()
-        videoPlayer!!.setOnErrorListener{
-            _, _,_ ->
-            Toast.makeText(applicationContext,"Getting error",Toast.LENGTH_SHORT).show()
-            false
-        }
-        videoPlayer!!.setOnCompletionListener {
-            Toast.makeText(applicationContext,"video completed",Toast.LENGTH_SHORT).show()
+            var videoView: VideoView=findViewById(R.id.videoView)
+            val mediaController=MediaController(this)
+            mediaController.setAnchorView(videoView)
+            videoView.setVideoPath("https://youtu.be/y8x3I0Ndt_E")
+            videoView.start()
+            videoView.setMediaController(mediaController)
         }
     }
-
-
 }
