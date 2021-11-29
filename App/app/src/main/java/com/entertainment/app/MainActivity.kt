f2b737f91366652f0a03e9b5796aa6e22e7eaf2d
Carlos Mata <carlosmata1009@gmail.com>
2021-11-29 20:10:23 +0100

    cambios

--- App/app/src/main/java/com/entertainment/app/MainActivity.kt
+++ App/app/src/main/java/com/entertainment/app/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.entertainment.app
 
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import android.widget.MediaController
-import android.widget.VideoView
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var log:MyLogActivity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        log.log("onCreate")
         supportActionBar?.hide()//Delete bar
         val button = findViewById<Button>(R.id.button5)
         button.setOnClickListener{
@@ -20,17 +22,16 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
-
     override fun onResume(){
         super.onResume()
-        Log.d("carlos","App on resume")
+        log.log("onResume")
     }
     override fun onPause(){
         super.onPause()
-        Log.d("carlos","App paused")
+        log.log("onPause")
     }
     override fun onDestroy(){
         super.onDestroy()
-        Log.d("carlos","Destroying")
+        log.log("onDestroy")
     }
 }
