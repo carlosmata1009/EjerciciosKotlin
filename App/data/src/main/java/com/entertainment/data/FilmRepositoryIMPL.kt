865be7426145ad04f8e090c8e2e81c1c297e7104
Carlos Mata <carlosmata1009@gmail.com>
2022-02-17 19:37:32 +0100

    //12.6

--- App/data/src/main/java/com/entertainment/data/FilmRepositoryIMPL.kt
+++ App/data/src/main/java/com/entertainment/data/FilmRepositoryIMPL.kt
@@ -5,21 +5,30 @@ import com.entertainment.domain.FilmRepository
 import javax.inject.Inject
 
 class FilmRepositoryIMPL @Inject constructor(
-    private val dataSource:ServerDataSource
+    private val dataSource:ServerDataSource,
+    private val databaseDataSource: DatabaseDatasource
     ): FilmRepository {
-    override suspend fun getFilm(id:Int,language: String): Film? {
-        try{
-            return dataSource.getFilm(id,language)
-        }catch (e:Exception){
-            return null
+
+    override suspend fun getFilm(filmPos: Int, language: String): Film? {
+        return runCatching {
+            val filmFromServer = dataSource.getFilm(filmPos, language)
+            databaseDataSource.updateFilmList(listOf(filmFromServer))
+            filmFromServer
+        }.getOrNull() ?: kotlin.run {
+            databaseDataSource.getFilm(filmPos)
         }
+
     }
 
-    override suspend fun getFilms(language: String): List<Film>? {
-        try{
-            return dataSource.getFilms(language)
-        }catch (e:Exception){
-            return null
+    override suspend fun getFilms(language: String): List<Film> {
+        return runCatching {
+            val filmListFromServer = dataSource.getFilms(language)
+            databaseDataSource.clearFilmList()
+            databaseDataSource.updateFilmList(filmListFromServer ?: emptyList())
+
+            filmListFromServer
+        }.getOrNull() ?: kotlin.run {
+            databaseDataSource.getFilmList()
         }
     }
 }
